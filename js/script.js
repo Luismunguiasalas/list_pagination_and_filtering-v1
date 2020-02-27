f161994b48b0bffd63b9bf86d3fdb716fa834a65
Luis <36023613+Luismunguiasalas@users.noreply.github.com>
2020-02-26 20:20:57 -0800

    update js

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ FSJS project 2 - List Filter and Pagination
    scoped to that function.
 ***/
 
-let studentListItem = document.querySelectorAll(".student-item")
+let studentList = document.querySelectorAll(".student-item")  //selects all elements with class .student-item
 const numberOfItemsOnPage = 10;
 
 /***
@@ -35,53 +35,98 @@ const numberOfItemsOnPage = 10;
        "invoke" the function
 ***/
 
-function showPage(studentList, page) {
+function showPage(stdntList, page) {
 
    let startIndex = (page * numberOfItemsOnPage) - numberOfItemsOnPage;
    let endIndex = page * numberOfItemsOnPage;
 
-   for (let index = 0; index < studentList.length; index++) {
-      studentList[index].style.display = "none";
+   for (let index = 0; index < stdntList.length; index++) {
+      stdntList[index].style.display = "none";   //changes the display property for each student Item,
       if (index >= startIndex && index < endIndex) {
-         studentList[index].style.display = "block";
+         stdntList[index].style.display = "block";
       }
    }
 
 }
 
-showPage(studentListItem, 1);
+showPage(studentList, 6);
 
 /***
    Create the `appendPageLinks function` to generate, append, and add
    functionality to the pagination buttons.
 ***/
 
-function appendPageLinks(studentList) {
-   let total = Math.floor(studentListItem.length / numberOfItemsOnPage);
-   // console.log(total);
-   let siblingElem = document.querySelector(".student-list");
-   // console.log(siblingElem);
-   siblingElem.insertAdjacentHTML("afterend", '<div class="pagination"></div>');
-   let paginationElem = document.querySelector(".pagination");
-   let string = `<ul>`;
 
+function appendPageLinks(stdntList) {
+   let totalPages = Math.ceil(stdntList.length / numberOfItemsOnPage); // get page number
+   let divContainer = document.querySelector(".page");  // get div.page container
+   let divPagination = document.createElement("div");   // create div pagination container
+   let nestedUL = document.createElement("ul");
+   // let liElement = document.createElement("li");
+   // let aElement = document.createElement("a");        // create nested ul element for pagination div
+   divPagination.className = "pagination";              // 
+   divPagination.append(nestedUL);
+   // nestedUL.append("") // testing nestedUL
+   // divContainer.append(divPagination);
 
+   for (let index = 0; index < totalPages; index++) {
+      let liElement = document.createElement("li");
+      let aElement = document.createElement("a");
+      // aElement.className = "active";
+      // aElement.onclick = ""
+      aElement.href = "#";
+      aElement.textContent = `${index + 1}`;
+      liElement.append(aElement);
+      nestedUL.append(liElement)
+   }
+   nestedUL.firstChild.firstChild.className = "active";
+   // document.querySelector(".pagination ul li a")
+   // let alll = document.querySelectorAll(".pagination ul li a")
 
-   // paginationElem.innerHTML += `<ul>`;
-
-   // paginationElem.innerHTML += `</ul>`;
-
-   for (let index = 0; index < total; index++) {
-
-      let element = document.createElement("li").innerHTML = `<li><a class="active" href="#">1</a></li>`
-      element.addEventListener()
-      string += `<li><a class="active" href="#">1</a></li>`
+   // alll.addEventListener("click", function () {
+   //    console.log("hello");
+   // });
+   for (let index = 0; index < totalPages; index++) {
+      nestedUL.firstChild.firstChild.addEventListener("click", function () {
+         console.log("hellow");
+      })
    }
 
+   // console.log(divContainer);   testing divContainer
 
+   // let total = Math.ceil(stdntList.length / numberOfItemsOnPage);
+   // console.log(total);  // testing total variable
+
+   divContainer.append(divPagination);
 }
 
-appendPageLinks(studentListItem);
+appendPageLinks(studentList);
+// function appendPageLinks(studentList) {
+//    let total = Math.floor(studentListItem.length / numberOfItemsOnPage);
+//    // console.log(total);
+//    let siblingElem = document.querySelector(".student-list");
+//    // console.log(siblingElem);
+//    siblingElem.insertAdjacentHTML("afterend", '<div class="pagination"></div>');
+//    let paginationElem = document.querySelector(".pagination");
+//    let string = `<ul>`;
+
+
+
+//    // paginationElem.innerHTML += `<ul>`;
+
+//    // paginationElem.innerHTML += `</ul>`;
+
+//    for (let index = 0; index < total; index++) {
+
+//       let element = document.createElement("li").innerHTML = `<li><a class="active" href="#">1</a></li>`
+//       element.addEventListener()
+//       string += `<li><a class="active" href="#">1</a></li>`
+//    }
+
+
+// }
+
+// appendPageLinks(studentListItem);
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
